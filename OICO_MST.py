e0a2a11b7b4efdc142e19f54c63fe2dbd2552963
hayder <hayder@modernecotech.com>
2019-03-03 10:03:22 +0000

    added gain, longer exposure time, higher brightness

--- OICO_MST.py
+++ OICO_MST.py
@@ -18,10 +18,37 @@ ser = serial.Serial(serialList[0])
 video_capture_1 = cv2.VideoCapture(1)
 video_capture_2 = cv2.VideoCapture(2)
 
-#set camera parameters
+"""set camera parameters
+
+0. CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
+1. CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
+2. CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
+3. CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
+4. CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
+5. CV_CAP_PROP_FPS Frame rate.
+6. CV_CAP_PROP_FOURCC 4-character code of codec.
+7. CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
+8. CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
+9. CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
+10. CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
+11. CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
+12. CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
+13. CV_CAP_PROP_HUE Hue of the image (only for cameras).
+14. CV_CAP_PROP_GAIN Gain of the image (only for cameras).
+15. CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
+16. CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
+17. CV_CAP_PROP_WHITE_BALANCE Currently unsupported
+18. CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
+
+"""
+#the camera for monochrome / IR images
 video_capture_1.set(3,1920)
 video_capture_1.set(4,1080)
+video_capture_1.set(10,100) #brightness
+video_capture_1.set(14,100) #gain
+video_capture_1.set(15,1000)#exposure
 
+#The camera for colour images
 video_capture_2.set(3,2592)
 video_capture_2.set(4,1944)
 
